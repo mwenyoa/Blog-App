f909870b3f93d9d989f1e1a76a4f9531abf869ff
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-10 15:28:21 +0200

    Add likes table

---
+++ db/migrate/20220810123339_create_likes.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# rubocop: nodoc
+class CreateLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.integer :authorid
+      t.integer :postid
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
