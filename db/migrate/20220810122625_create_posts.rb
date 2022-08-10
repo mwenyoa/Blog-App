97f873bbad91ddd6a5708f86208cb8b97f179719
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-10 15:27:36 +0200

    create database relations

---
+++ db/migrate/20220810122625_create_posts.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# rubocop: nodoc
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.integer :authorid
+      t.string :title
+      t.text :text
+      t.string :comments_counter
+      t.string :likes_counter
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
