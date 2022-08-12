084f9083fd06d0e68989c240446764518a495d1e
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-12 20:10:18 +0200

    validate name and counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'Comment'
   has_many :likes, class_name: 'Like'
 
+  # user validations
+  validates :name, presence: true
+  validates :posts_counter,  numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   def recent_posts
     posts.limit(3).order(created_at: :desc)
   end
