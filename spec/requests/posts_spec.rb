6abdaf6110bce9aff18a8fa25c319874a1ad2e84
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-16 06:16:28 +0200

    Add sepcs for the posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,12 +6,32 @@ RSpec.describe 'Posts', type: :request do
       get '/posts/index'
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders index template' do
+      get '/posts/index'
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder text' do
+      get '/posts/index'
+      expect(response.body).to include('Many User Posts')
+    end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
       get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders show template' do
+      get '/posts/show'
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes correct placeholder text' do
+      get '/posts/show'
+      expect(response.body).to include('Signle Post')
     end
   end
 end
