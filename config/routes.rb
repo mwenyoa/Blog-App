d6440f505159bf328a3f82c36e6308bb305d769e
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-19 21:19:08 +0200

    Nest routes in posts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/index'
-  get 'comments/create'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
-  get 'likes/create'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'users#index'
 
-  resources :comments, only: [:new, :create]
-  resources :likes, only: [:create]
-
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create]
+  end
+
+  resources :posts do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
   end
 end
