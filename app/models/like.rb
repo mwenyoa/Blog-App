2af9e12b71bf969a4a00d792d43377bab9c3550a
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-19 10:53:46 +0200

    Add validations to likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,13 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
 
+  # validations
+  validates :author_id, presence: true
+  validates :post_id, presence: true
+
+  after_save :update_likes_counter
+
   def update_likes_counter
-    post.increment!(likes_counter)
+    post.increment!(:likes_counter)
   end
 end
