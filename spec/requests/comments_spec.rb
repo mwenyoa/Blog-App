1f25e90a86f814d07a150f05dbe52e13ae0838bf
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-19 11:11:12 +0200

    fix linter errors

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/comments/create"
+RSpec.describe 'Comments', type: :request do
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/comments/create'
       expect(response).to have_http_status(:success)
     end
   end
--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Likes", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/likes/create"
+RSpec.describe 'Likes', type: :request do
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/likes/create'
       expect(response).to have_http_status(:success)
     end
   end
