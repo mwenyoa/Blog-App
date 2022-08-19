34836da1280efd70477a1733d6f58ed5f5086a19
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-19 10:50:18 +0200

    Add logic to posts controller to add comments and likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,27 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @like = Like.new
+  end
+
+  def new
+    @post = Post.new
+    @params = params
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to user_post_path(id: @post.id, user_id: @post.author_id), notice: 'Post was successfully created.'
+    else
+      render :new, alert: 'Error occurred, Post not saved'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :author_id)
   end
 end
